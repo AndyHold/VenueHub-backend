e7bf869eb766599a96547e7418f56380ac6baae6
Andy Holden <ash102@uclive.ac.nz>
2019-03-03 13:52:59 +1300

    fault finding for x-auth headers

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -46,22 +46,23 @@ exports.logout = function(req, res) {
 };
 
 exports.getUser = function(req, res) {
-    // Get the bearer from the request headers
-    const authToken = req.headers["x-authorization"];
-    // Parse the id from the request parameters
-    let id = req.params.id;
-    // Call the model class to do the database querying and logic
-    Users.getUser(id, authToken, function(code, userObj) {
-        // If the response code is an error
-        if (code === 404) {
-            // Send the response code
-            res.sendStatus(code);
-            // If the response code is a success
-        } else if (code === 200) {
-            // Send the code along with the user object
-            res.status(code).send(userObj);
-        }
-    });
+    // // Get the bearer from the request headers
+    // const authToken = req.headers["x-authorization"];
+    // // Parse the id from the request parameters
+    // let id = req.params.id;
+    // // Call the model class to do the database querying and logic
+    // Users.getUser(id, authToken, function(code, userObj) {
+    //     // If the response code is an error
+    //     if (code === 404) {
+    //         // Send the response code
+    //         res.sendStatus(code);
+    //         // If the response code is a success
+    //     } else if (code === 200) {
+    //         // Send the code along with the user object
+    //         res.status(code).send(userObj);
+    //     }
+    // });
+    res.status(200).send({"email": req.headers})
 };
 
 exports.updateUser = function(req, res) {
