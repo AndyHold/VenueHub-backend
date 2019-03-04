ef4a62471b829e77b16b6acda76dd801a17cc613
Andy Holden <ash102@uclive.ac.nz>
2019-03-04 17:43:49 +1300

    Implemented the get user photo functionality.

--- app/controllers/users.photos.controller.js
+++ app/controllers/users.photos.controller.js
@@ -1,12 +1,20 @@
 const Photos = require('../models/users.photos.model.js');
 
 exports.getPhoto = function(req, res) {
-    // let id = req.params.id;
-    // // req.accessToken;
-    // Photos.getPhoto(id, function(result) {
-    //     res.json(result);
-    // });
-    res.sendStatus(200);
+    // Get the user id from the params
+    let userId = parseInt(req.params.id);
+    // Call the model class to do the logic and call the database
+    Photos.getPhoto(id, function(code, result) {
+        // If the code is 404
+        if (code === 404) {
+            // Send the code via the response
+            res.sendStatus(code);
+            // Otherwise
+        } else {
+            // Send the code and the resulting object
+            res.status(code).send(result); // TODO not sure if this is correct...?
+        }
+    });
 };
 
 exports.setPhoto = function(req, res) {
--- app/models/users.photos.model.js
+++ app/models/users.photos.model.js
@@ -1,10 +1,27 @@
 const db = require('../../config/db');
 const filesystem = require("fs");
 
-exports.getPhoto = function(id, done) {
-    db.get_pool().query('SELECT profile_photo_filename FROM User WHERE user_id = ?', [id], function (err, rows) {
-
-        if (err || !rows[0].hasOwnProperty("profile_photo_filename")) return done({404: "Not Found"});
+exports.getPhoto = function(userId, done) {
+    // Call the database to retrieve the filename of the users photo
+    db.getPool().query('SELECT profile_photo_filename AS filename FROM User WHERE user_id=?', [userId], function (err, rows) {
+        // If the database returns an error or the rows are empty
+        if (err || rows.length === 0) {
+            // Return the done function with a 404 - Not Found code
+            return done(404);
+            // Otherwise
+        } else {
+            // Call the filesystem to read the file
+            filesystem.readFile("./photos/" + rows[0]["filename"], function (err, data) {
+                // If the filesystem returned an error
+                if (err) {
+                    // Return the done function with a 404 - Not Found code
+                    return done(404);
+                } else {
+                    // Return the done function with a 200 - OK code and the contents of the picture
+                    return done(200, data);
+                }
+            });
+        }
 
         return done({200:filesystem.readFile(rows[0]['profile_photo_filename'])});
     });
@@ -15,7 +32,7 @@ exports.setPhoto = function(id, photo, done) {
     // create a random filename instead of the given one as there are security risks.
         // delete it and replace it with the received one
     // else save the new photo
-    db.get_pool().query('', function (err, rows) {
+    db.getPool().query('', function (err, rows) {
 
         if (err) return done({"ERROR": "Error selecting"});
 
@@ -25,7 +42,7 @@ exports.setPhoto = function(id, photo, done) {
 
 exports.deletePhoto = function(done) {
     // Check that the userId corresponds with the currently logged in user.
-    db.get_pool().query('', function (err, rows) {
+    db.getPool().query('', function (err, rows) {
 
         if (err) return done({"ERROR": "Error selecting"});
 
