3878610a8e6a55c073743580a73eac53169f5028
Andy Holden <ash102@uclive.ac.nz>
2019-03-04 13:31:39 +1300

    Implemented the hashing function for the backdoor

--- app/models/backdoor.model.js
+++ app/models/backdoor.model.js
@@ -1,5 +1,6 @@
 const db = require('../../config/db');
 const fs = require('mz/fs');
+const crypt = require("bcrypt");
 
 const photoDirectory = './storage/photos/';
 
@@ -58,8 +59,11 @@ async function populateDefaultUsers() {
 
 async function changePasswordToHash(user, passwordIndex) {
     // TODO you need to implement "passwords.hash()" yourself, then uncomment the line below.
-    // user[passwordIndex] = await passwords.hash(user[passwordIndex]);
-
+    let passwords = {"hash" : function (password) {
+            return crypt.hashSync(password, 10);
+        }
+    }
+    user[passwordIndex] = await passwords.hash(user[passwordIndex]);
     // It is recommended you use a reputable cryptology library to do the actual hashing/comparing for you...
 }
 
